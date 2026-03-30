9ed90d9b0516be2ed44723d7478ea5a724fbb5f5
darksim33 <57491748+darksim33@users.noreply.github.com>
2026-03-30 16:08:53 +0200

    feat: Include __version__ in the module's __all__ export

--- pyneapple_gpufit/__init__.py
+++ pyneapple_gpufit/__init__.py
@@ -5,4 +5,6 @@ from __future__ import annotations
 from .curvefit_solver import GpuCurveFitSolver
 from .nnls_solver import GpuNNLSSolver
 
-__all__ = ["GpuCurveFitSolver", "GpuNNLSSolver"]
+__version__ = "0.1.0"
+
+__all__ = ["GpuCurveFitSolver", "GpuNNLSSolver", "__version__"]
